f1e8f5691f7fdd01d5a8c97cfaa87898bf680a8b
Jim Studt <jim@studt.net>
2021-03-18 19:34:06 -0500

    Found reset_usb_boot(), get rid of flash wiping code

--- src/wipe.c
+++ src/wipe.c
@@ -1,24 +1,7 @@
 
 #include "wipe.h"
-#include "hardware/watchdog.h"
-#include "hardware/flash.h"
-#include "hardware/sync.h"
-#include "pico/multicore.h"
+#include "pico/bootrom.h"
 
 void wipe(const char *line) {
-    const uint8_t blank[256] = {0};
-
-    // Stop core1. We better be core0.
-    multicore_reset_core1();                  
-	
-    uint32_t saved = save_and_disable_interrupts();
-    // Wipe the secondary boot loader
-    flash_range_program( 0, blank, 256);      
-    restore_interrupts(saved);
-	
-    // this will reset us in 1 millisecond.
-    watchdog_enable( 1, false);
-    
-    // await our doom
-    while( true);
+    reset_usb_boot(0,0);
 }
